0258f598caea94010d87bf94b3bd344f9c4769f6
Marcos <marcos23589@gmail.com>
2022-01-25 21:06:54 -0300

    [add] personajeSchema - [refactor] vista add

--- controllers/controladorPrincipal.js
+++ controllers/controladorPrincipal.js
@@ -1,86 +1,154 @@
-const { Error } = require('mongoose');
-const Personaje = require('../models/personajes');
-const helpers = require('../lib/helpers');
+const { Error } = require("mongoose");
+const Personaje = require("../models/personajes");
+const helpers = require("../lib/helpers");
 
-exports.paramGet = (req, res) => {    
-    const {mivariable, otravariable} = req.params;
-    return res.send(`Las variables son ${mivariable} y ${otravariable}`);
-}
-
-exports.paginaHome = (req, res) =>{    
-    return res.render('home',{nombrePagina:'Home ceroUno'});
+exports.paramGet = (req, res) => {
+  const { mivariable, otravariable } = req.params;
+  return res.send(`Las variables son ${mivariable} y ${otravariable}`);
 };
 
+exports.paginaHome = (req, res) => {
+  return res.render("home", { nombrePagina: "Home ceroUno" });
+};
 
-exports.registrarDB = (req, res) =>{
-    return res.send('Registro en DB')
-}
+exports.registrarDB = (req, res) => {
+  return res.send("Registro en DB");
+};
 
-exports.crearPersonajeGet = (req,res) => {
-    return res.render('alumnos/add',{nombrePagina:'Home ceroUno'});
-}
+exports.crearPersonajeGet = (req, res) => {
+  return res.render("alumnos/add", { nombrePagina: "Agregar alumno" });
+};
 
-exports.crearPersonaje = async (req,res)=>{
-    
-    const persona = Personaje(req.body);
+exports.crearPersonaje = async (req, res) => {
+  const {
+    nombre,
+    apellido,
+    lugarNacimiento,
+    documento,
+    nacimiento,
+    curso,
+    anioCurso,
+    numeroLibro,
+    folioLibro,
+    numeroLegajo,
+    anioLegajo,
+    colegioOrigen,
+    ingreso,
+    colegioDestino,
+    fechaSalida,
+    situacion,
+    fechaEgreso,
+    observaciones,
+  } = req.body;
 
-    try {        
-        await persona.save();        
-        req.flash('info', 'Ingreso exitoso!');
-    } catch (error) {        
-        console.log("ERROR! ->",error);
-        if(error.code == 11000){
-            req.flash('alerta', 'Documento ya registrado!');
-            return res.redirect('list');
-        }
-    }    
-    return res.redirect('list');    
-}
+  const libroMatriz = { numeroLibro, folioLibro };
+  const legajo = { numeroLegajo, anioLegajo };
+  let trayectoria = new Array();
+  trayectoria.push({ curso, anioCurso });
+  const historial = {
+    colegioOrigen,
+    ingreso,
+    colegioDestino,
+    fechaSalida,
+    situacion,
+    fechaEgreso,
+    observaciones,
+  };
 
-exports.listarPersonajes = async (req,res)=>{
-    const resultado = await Personaje.find();
-    
-    for(let i=0; i<resultado.length; i++){
-        let fecha = helpers.fechaNacimiento(resultado[i].nacimiento);
-        //se crea otra variable dentro del objeto para mostrarla
-        resultado[i].fecha = fecha;        
+  let persona = {
+    nombre,
+    apellido,
+    documento,
+    nacimiento,
+    lugarNacimiento,
+    libroMatriz,
+    legajo,
+    trayectoria,
+    historial,
+  };
+
+  console.log("crear ->", persona);
+
+  try {
+    await Personaje(persona).save();
+    req.flash("info", "Ingreso exitoso!");
+  } catch (error) {
+    console.log("ERROR! ->", error);
+    if (error.code == 11000) {
+      req.flash("alerta", "Documento ya registrado!");
+      return res.redirect("list");
     }
-    
-    return res.render('alumnos/list', {resultado, nombrePagina:'Lista de agentes'})
-}
+  }
+  return res.redirect("list");
+};
 
-exports.eliminarPersonajes = async (req, res) =>{            
-    await Personaje.findByIdAndDelete(req.params._id);
-    req.flash('alerta', 'Registro eliminado!');
-    return res.redirect('/list')
-}
+exports.listarPersonajes = async (req, res) => {
+  const resultado = await Personaje.find();
+  for (let i = 0; i < resultado.length; i++) {
+    let fecha = helpers.fechaNacimiento(resultado[i].nacimiento);
+    //se crea otra variable dentro del objeto para mostrarla
+    resultado[i].fecha = fecha;
+  }
 
-exports.editarPersonaje = async (req, res) =>{
-    const idEdicion = req.params._id;
-    const edicion = await Personaje.findById(idEdicion);    
-    let fecha = helpers.fechaNacimientoBacks(edicion.nacimiento);
-        
-    return res.render('alumnos/edit',{edicion, fecha, nombrePagina:'Home ceroUno'})
-}
+  return res.render("alumnos/list", {
+    resultado,
+    nombrePagina: "Lista de alumnos",
+  });
+};
 
-exports.postPersonaje = async (req, res)=>{    
-    const personajeId = req.params._id   
-    const editado = req.body;
+exports.eliminarPersonajes = async (req, res) => {
+  await Personaje.findByIdAndDelete(req.params._id);
+  req.flash("alerta", "Registro eliminado!");
+  return res.redirect("/list");
+};
 
-    await Personaje.findByIdAndUpdate(personajeId,{
-        nombre: editado.nombre,
-        apellido: editado.apellido, 
-        documento: editado.documento, 
-        nacimiento: editado.nacimiento
-    });
-    req.flash('info', 'Edición exitosa!');
-    return res.redirect('/list')
-}
+exports.editarPersonaje = async (req, res) => {
+  const idEdicion = req.params._id;
+  const edicion = await Personaje.findById(idEdicion);
+  let fecha = helpers.fechaNacimientoBacks(edicion.nacimiento);
 
-exports.verPersonaje = async (req,res) =>{
-    const idVista = req.params._id;
-    const vista = await Personaje.findById(idVista)
-    const fechaNac = helpers.fechaNacimiento(vista.nacimiento)
-    console.log("idvista ->",vista);
-    return res.render('alumnos/view', {vista, fechaNac, nombrePagina:'Home ceroUno'})
-}
+  return res.render("alumnos/edit", {
+    edicion,
+    fecha,
+    nombrePagina: "Home",
+  });
+};
+
+exports.postPersonaje = async (req, res) => {
+  const personajeId = req.params._id;
+  const editado = req.body;
+
+  await Personaje.findByIdAndUpdate(personajeId, {
+    nombre: editado.nombre,
+    apellido: editado.apellido,
+    documento: editado.documento,
+    nacimiento: editado.nacimiento,
+  });
+  req.flash("info", "Edición exitosa!");
+  return res.redirect("/list");
+};
+
+exports.verPersonaje = async (req, res) => {
+  const idVista = req.params._id;
+  const vista = await Personaje.findById(idVista);
+  const fechaNac = helpers.fechaNacimiento(vista.nacimiento);
+  const fechaIngreso = helpers.fechaNacimiento(vista.historial.ingreso);
+  const fechaSalida = helpers.fechaNacimiento(vista.historial.fechaSalida);
+  const fechaEgreso = helpers.fechaNacimiento(vista.historial.fechaEgreso);
+  let historial = vista.historial;
+  let trayectoria = vista.trayectoria;
+
+  console.log("idvista ->", fechaNac);
+  console.log("idvista2 ->", fechaIngreso);
+
+  return res.render("alumnos/view", {
+    vista,
+    fechaNac,
+    fechaIngreso,
+    fechaEgreso,
+    fechaSalida,
+    historial,
+    trayectoria,
+    nombrePagina: "Vista",
+  });
+};
--- models/personajes.js
+++ models/personajes.js
@@ -1,16 +1,29 @@
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 
 const personajeSchema = mongoose.Schema({
-    nombre: String,
-    apellido: String,        
-    documento: {type: Number, required: true, unique: true},
-    nacimiento: Date
+  nombre: String,
+  apellido: String,
+  documento: { type: Number, required: true, unique: true },
+  nacimiento: Date,
+  lugarNacimiento: String,
+  libroMatriz: { numeroLibro: Number, folioLibro: Number },
+  legajo: { numeroLegajo: Number, anioLegajo: Number },
+  trayectoria: new Array({ curso: String, anioCurso: Number }),
+  historial: {
+    colegioOrigen: String,
+    ingreso: Date,
+    colegioDestino: String,
+    fechaSalida: Date,
+    situacion: String,
+    observaciones: String,
+    fechaEgreso: Date,
+  },
 });
 
-const modeloPersonaje = mongoose.model('Personaje', personajeSchema);
+const modeloPersonaje = mongoose.model("Personaje", personajeSchema);
 
-modeloPersonaje.on('index', (error)=>{
-    console.log("error ->", error)
-})
+modeloPersonaje.on("index", (error) => {
+  console.log("error ->", error);
+});
 
 module.exports = modeloPersonaje;
