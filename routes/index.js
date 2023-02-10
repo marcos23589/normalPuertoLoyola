c8cfc1607350fd876e0652a355a7a1aff4364795
marcos <marcos23589@gmail.com>
2023-02-10 11:32:31 -0300

    [FIX] Correciones varias

--- routes/index.js
+++ routes/index.js
@@ -7,28 +7,29 @@ const controladorPrincipal = require('../controllers/controlador');
 app.use('/', router()); en server.js */
 
 module.exports = (req, res) => {
+  //en la raiz
+  router.get("/", controladorPrincipal.paginaHome);
 
-    //en la raiz
-    router.get('/', controladorPrincipal.paginaHome);
+  router.get(
+    "/conparametrosget/:mivariable/:variable2",
+    controladorPrincipal.paramGet
+  );
 
-    router.get('/conparametrosget/:mivariable/:variable2', 
-                controladorPrincipal.paramGet);
+  router.get("/about", controladorPrincipal.acerca);
 
-    router.get('/about', controladorPrincipal.acerca)
+  router.get("/add", controladorPrincipal.crearPersonajeGet);
 
-    router.get('/add', controladorPrincipal.crearPersonajeGet)
+  router.post("/add", controladorPrincipal.crearPersonaje);
 
-    router.post('/add', controladorPrincipal.crearPersonaje)
+  router.get("/list", controladorPrincipal.listarPersonajes);
 
-    router.get('/list', controladorPrincipal.listarPersonajes)
+  router.get("/delete/:_id", controladorPrincipal.eliminarPersonajes);
 
-    router.get('/delete/:_id', controladorPrincipal.eliminarPersonajes)
+  router.get("/edit/:_id", controladorPrincipal.editarPersonaje);
 
-    router.get('/edit/:_id', controladorPrincipal.editarPersonaje)
+  router.post("/edit/:_id", controladorPrincipal.postPersonaje);
 
-    router.post('/edit/:_id', controladorPrincipal.postPersonaje)
+  router.get("/view/:_id", controladorPrincipal.verPersonaje);
 
-    router.get('/view/:_id', controladorPrincipal.verPersonaje)
-
-    return router;
+  return router;
 };
