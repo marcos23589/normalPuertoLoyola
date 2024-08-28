b2cb22e2b53157d0e5060d2b737f34604f4eb2d9
marcos <marcos23589@gmail.com>
2024-08-28 18:49:56 -0300

    [add]

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
-const mongoose = require ('mongoose');
-const uriDataBase = 'mongodb://localhost:27017/';
-const nombreDatabase = 'secundarioDB';
+const mongoose = require("mongoose");
+const uriDataBase = "mongodb://localhost:27017/";
+const nombreDatabase = "secundarioDB";
 
+mongoose.set("strictQuery", true);
 //establezco la conexion con la base de datos
 mongoose.connect(`${uriDataBase}${nombreDatabase}`);
 
 //si se conecta
-mongoose.connection.once('open', () =>{
-    console.log(`Conexion exitosa a ${nombreDatabase}`);
-})
+mongoose.connection.once("open", () => {
+  console.log(`Conexion exitosa a ${nombreDatabase}`);
+});
 
 //si algo falla
-mongoose.connection.on('error', (error)=>{
-    console.log(`Ha ocurrido el siguiente error: ${error}`);
-})
+mongoose.connection.on("error", (error) => {
+  console.log(`Ha ocurrido el siguiente error: ${error}`);
+});
